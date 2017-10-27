ba44eca6b4328789ec6fc14441407103b0a2efd3
William Smith <williampsmith@berkeley.edu>
2017-10-26 23:30:45 -0700

    finish CrowdSale contract implementation

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -19,8 +19,19 @@ contract Crowdsale {
   Token private token;
   Queue public q;
 
+  event PurchaseCompleted(address buyer, bool successful);
+  event RefundCompleted(address buyer, bool successful);
+
   modifier OwnerOnly() {
 		if (msg.sender == owner) {_;}
+	}
+
+  modifier SaleHasEnded() {
+		if (now > startTime) {_;}
+	}
+
+  modifier SaleHasNotEnded() {
+		if (now <= startTime) {_;}
 	}
 
   function Crowdsale(uint256 _exhangeRate, uint256 totalSupply, uint timeCap) {
@@ -32,21 +43,54 @@ contract Crowdsale {
     q = new Queue();
   }
 
-  function mint(uint256 amount) OwnerOnly() returns (bool) {
-    token.totalSupply += amount;
-    return true;
+  function mint(uint256 amount) OwnerOnly() {
+    token.mint(amount);
   }
 
   function burn(uint256 amount) OwnerOnly() returns (bool) {
-    token.totalSupply -= amount;
-    return true;
+    if ((token.totalSupply - tokensSold) >= amount) {
+      token.totalSupply -= amount;
+      return true;
+    }
+    return false;
   }
 
-  function sell() payable returns (bool) {
-    // TODO
+  function sell() payable SaleHasNotEnded() returns (bool) {
+    uint sellTime = now;
+    uint256 tokensPurchased = msg.value * exchangeRate;
+
+    if (tokensPurchased > (totalSupply - tokensSold)) {
+      return false;
+    }
+
+    q.enqueue(msg.sender);
+    uint8 place = q.checkPlace();
+    while (place > 1) {
+      place = checkPlace();
+    }
+    assert(q.checkPlace > 0); // should NEVER happen
+
+    q.checkTime();
+    if (q.checkPlace() == 0) { // timeout
+      PurchaseCompleted(msg.sender, false);
+      return false;
+    }
+
+    bool success = token.transfer(msg.sender, tokensPurchased);
+    PurchaseCompleted(msg.sender, success);
+    return success;
   }
 
-  function refund() returns (bool) {
-    // TODO
+  function refund(uint256 amount) SaleHasNotEnded() returns (bool) {
+    success = token.refund(msg.sender, amount);
+    if (success) {
+      success = msg.sender.send(amount);
+    }
+    RefundCompleted(msg.sender, success);
+    return success;
+  }
+
+  function receiveFunds() SaleHasEnded() OwnerOnly() returns (bool) {
+    return owner.send(crowdSaleBalance);
   }
 }
