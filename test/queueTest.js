f3a21321e48154ac12c514e82e0326d91784a9b0
arissa@berkeley.edu <arissa@berkeley.edu>
2017-10-28 02:39:35 -0700

    added advanced tests and some more basic tests

--- test/queueTest.js
+++ test/queueTest.js
@@ -7,7 +7,7 @@ contract('queueTest', function(accounts) {
 	/* Define your constant variables and instantiate constantly changing 
 	 * ones
 	 */
-	const args = {alice: 0x0001, bob: 0x0002, carlos: 0x0003, dave: 0x0004, eve:0x0005, frank: 0x0006};
+	const args = {};
 	let queue;
 
 	/* Do something before every `describe` method */
@@ -26,22 +26,64 @@ contract('queueTest', function(accounts) {
 		});
 
 		it("Add a person to the queue and make sure the queue's size is correct.", async function() {
-			await queue.enqueue(args.alice);
+			await queue.enqueue(accounts[0]);
 			let queueSize = await queue.qsize();
 			assert.equal(queueSize, 1, "The queue should have a size of 1.");
 		});
 
 		it("Remove a person from the queue and make sure the queue's size is correct.", async function() {
-			await queue.enqueue(args.alice);
+			await queue.enqueue(accounts[0]);
 			await queue.dequeue();
 			let queueSize = await queue.qsize();
 			let isEmpty = await queue.empty();
 			assert.equal(queueSize, 0, "The queue should have a size of 0.");
 			assert.equal(isEmpty, true, "The queue should be empty.");
 		});
+
+		it("Check a person's place in the queue.", async function() {
+			await queue.enqueue(accounts[0]);
+			let pos = await queue.checkPlace.call({from: accounts[0]}).valueOf();
+			assert.equal(pos, 1, "The person's position should be 1, but instead was " + pos);
+		});
+
+		it("Get the person in the front of the queue.", async function() {
+			await queue.enqueue(accounts[0]);
+			await queue.enqueue(accounts[1]);
+			let first = await queue.getFirst();
+			assert.equal(first, accounts[0], "The wrong person was retrieved from the front of the queue.");
+		});
 	});
 
 	describe('Advanced Functionality', function() {
-		// YOUR CODE HERE
+		it("Add 5 people to the queue and make sure nobody else can be added to the queue.", async function() {
+			// add 5 people to the queue.
+			var i;
+			for (i = 0; i < 5; i++) {
+				await queue.enqueue(accounts[0]);
+			}
+			var queueSize = await queue.qsize();
+			assert.equal(queueSize, 5, "The queue should have a size of 5.");
+
+			// attempt to add a 6th person to the queue. the queue's size should still be 5
+			await queue.enqueue(accounts[5])
+			assert.equal(queueSize, 5, "The queue should have a size of 5 after failing to add a 6th person.");
+
+			var pos = await queue.checkPlace.call({from: accounts[5]});
+			assert.equal(pos.valueOf(), 0, "The 6th person should not have a place in the queue.");
+		});
+
+		// this test doesn't work yet since i need to figure out timing.
+		// it("Expel the person in the front of the queue.", async function() {
+		// 	await queue.enqueue(accounts[0]);
+		// 	await queue.enqueue(accounts[1]);
+		// 	await queue.checkTime();
+		// 	let queueSize = await queue.qsize();
+		// 	assert.equal(queueSize, 1, "The queue should have a size of 1, but was " + queueSize);
+		// 	var pos = await queue.checkPlace.call({from: accounts[0]}).valueOf();
+		// 	assert.equal(pos, 0, "The expelled person should no longer be in the queue.");
+		// 	pos = await queue.checkPlace.call({from: accounts[1]}).valueOf();
+		// 	assert.equal(pos, 1, "The correct person is not in front of the queue.");
+
+		// });
 	});
 });
